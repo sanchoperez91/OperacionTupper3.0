28c0d0feacbbfe10e7e7ffa37472c784a6339107
Javier <sancho.perez91@gmail.com>
2026-07-09 20:33:58 +0200

    ignore

--- OperacionTupper3.0/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OperacionTupper3._0.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
